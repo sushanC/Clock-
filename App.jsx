7fd703cb3c80983fc2c50a997d9cf91796b75133
Sushan Achar <sushanachar2006@gmail.com>
2025-06-08 20:28:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function LiveClock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    // Cleanup to avoid memory leak
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', fontSize: '2rem', marginTop: '50px' }}>
+      <p>🕒 Current Time:</p>
+      <h1>{time}</h1>
+    </div>
+  );
+}
+
+export default LiveClock;
